bbb42a5e852d4c599aa93c57307d8dff866e3283
a.kroeger <a.kroeger@grimme.de>
2023-07-14 11:31:29 +0200

    feat(lib): moved to lib

--- CLI-Menu/CLI-Menu/Program.cs
+++ CLI-Menu/CLI-Menu/Program.cs
@@ -1,15 +1,12 @@
-﻿namespace CLIMenu
+﻿namespace CLI.Menu.Example
 {
     internal class Program
     {
-        const int START_INDEX_Y = 1;
-        const int START_INDEX_X = 1;
-
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
 
-            int option = SelectOption("Kontostand abfragen", "Einzahlung", "Auszahlung", "Überweisung", "Kontoauszuüge drucken", "Beenden");
+            int option = CLI.SelectOption("Kontostand abfragen", "Einzahlung", "Auszahlung", "Überweisung", "Kontoauszuüge drucken", "Beenden");
 
             switch (option)
             {
@@ -34,98 +31,6 @@
                 default:
                     break;
             }
-        }
-
-        private static int SelectOption(params string[] items)
-        {
-            Console.Clear();
-            Console.WriteLine("Bitte auswählen:");
-
-            foreach (var item in items)
-            {
-                Console.WriteLine($"[ ] - {item}");
-            }
-
-            var option = ChooseOption(items.Length);
-
-            Console.Clear();
-
-            return option;
-        }
-
-        private static int ChooseOption(int optionCount)
-        {
-            bool selected = false;
-            int selectedIndex = 0;
-
-            WriteAt("X", 0, selectedIndex);
-
-            do
-            {
-                ConsoleKeyInfo result = Console.ReadKey(true);
-                int currentIndex = selectedIndex;
-
-                switch (result.Key)
-                {
-                    case ConsoleKey.UpArrow:
-                        if (selectedIndex == 0)
-                        {
-                            selectedIndex = optionCount - 1;
-                        }
-                        else
-                        {
-                            selectedIndex--;
-                        }
-                        MoveCursor(currentIndex, selectedIndex);
-                        break;
-                    case ConsoleKey.DownArrow:
-                        if (selectedIndex == optionCount - 1)
-                        {
-                            selectedIndex = 0;
-                        }
-                        else
-                        {
-                            selectedIndex++;
-                        }
-
-                        MoveCursor(currentIndex, selectedIndex);
-                        break;
-                    case ConsoleKey.Enter:
-                        selected = true;
-                        break;
-                    default:
-                        break;
-                }
-
-            } while (!selected);
-
-            return selectedIndex;
-        }
-
-        private static void MoveCursor(int currentIndex, int newIndex)
-        {
-            WriteAt(" ", 0, currentIndex);
-            WriteAt("X", 0, newIndex);
-            SetCursor(0, newIndex);
-        }
-
-        private static void WriteAt(string s, int x, int y)
-        {
-            try
-            {
-                SetCursor(x, y);
-                Console.Write(s);
-            }
-            catch (ArgumentOutOfRangeException e)
-            {
-                Console.Clear();
-                Console.WriteLine(e.Message);
-            }
-        }
-
-        private static void SetCursor(int x, int y)
-        {
-            Console.SetCursorPosition(START_INDEX_X + x, START_INDEX_Y + y);
         }
     }
 }
---
+++ CLI-Menu/CLI.Menu/CLI.cs
@@ -0,0 +1,100 @@
+﻿namespace CLI.Menu
+{
+    public static class CLI
+    {
+        const int START_INDEX_Y = 1;
+        const int START_INDEX_X = 1;
+
+        public static int SelectOption(params string[] items)
+        {
+            Console.Clear();
+            Console.WriteLine("Bitte auswählen:");
+
+            foreach (var item in items)
+            {
+                Console.WriteLine($"[ ] - {item}");
+            }
+
+            var option = ChooseOption(items.Length);
+
+            Console.Clear();
+
+            return option;
+        }
+
+        private static int ChooseOption(int optionCount)
+        {
+            bool selected = false;
+            int selectedIndex = 0;
+
+            WriteAt("X", 0, selectedIndex);
+
+            do
+            {
+                ConsoleKeyInfo result = Console.ReadKey(true);
+                int currentIndex = selectedIndex;
+
+                switch (result.Key)
+                {
+                    case ConsoleKey.UpArrow:
+                        if (selectedIndex == 0)
+                        {
+                            selectedIndex = optionCount - 1;
+                        }
+                        else
+                        {
+                            selectedIndex--;
+                        }
+                        MoveCursor(currentIndex, selectedIndex);
+                        break;
+                    case ConsoleKey.DownArrow:
+                        if (selectedIndex == optionCount - 1)
+                        {
+                            selectedIndex = 0;
+                        }
+                        else
+                        {
+                            selectedIndex++;
+                        }
+
+                        MoveCursor(currentIndex, selectedIndex);
+                        break;
+                    case ConsoleKey.Enter:
+                        selected = true;
+                        break;
+                    default:
+                        break;
+                }
+
+            } while (!selected);
+
+            return selectedIndex;
+        }
+
+        private static void MoveCursor(int currentIndex, int newIndex)
+        {
+            WriteAt(" ", 0, currentIndex);
+            WriteAt("X", 0, newIndex);
+            SetCursor(0, newIndex);
+        }
+
+        private static void WriteAt(string s, int x, int y)
+        {
+            try
+            {
+                SetCursor(x, y);
+                Console.Write(s);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.Clear();
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        private static void SetCursor(int x, int y)
+        {
+            Console.SetCursorPosition(START_INDEX_X + x, START_INDEX_Y + y);
+        }
+    }
+}
